2f90041a5ce22e71f7701f2180b2a291c0801535
Tri Nguyen <tri@tridnguyen.com>
2015-11-10 17:55:50 -0500

    try gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 
-require('babel-core/register');
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
 	return gulp.src('test/*.js', {read: false})
+		.pipe(babel({
+			presets: ['es2015']
+		}))
 		.pipe(mocha({
-			ui: 'tdd',
-			compilers: ['js:babel-core/register']
+			ui: 'tdd'
 		}));
 });

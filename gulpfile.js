a9989b6eda05dca33259cc6a44574c86a54fb6eb
Tri Nguyen <tri@tridnguyen.com>
2015-12-07 13:39:02 -0500

    use babel-polyfill fixes the problem

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var mocha = require('gulp-mocha');
+require('babel-core/register');
+require('babel-polyfill');
 
 gulp.task('test', function () {
 	return gulp.src('test/*.js', {read: false})
-		.pipe(babel({
-			presets: ['es2015']
-		}))
+		.pipe(babel())
 		.pipe(mocha({
 			ui: 'tdd'
 		}));
